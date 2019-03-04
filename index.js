f2eb126067b251df04df00fff2f7ff3c946fd012
Justine B <justinegayton@gmail.com>
2019-03-04 20:57:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(2, 4);}
 
 const selectingDrivers = function(drivers) {
-  return returnFirstTwoDrivers;
+  return returnFirstTwoDrivers();
   return returnLastTwoDrivers;
 }
   
